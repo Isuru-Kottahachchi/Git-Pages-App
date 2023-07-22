78210e6d17fec38932e9b825e06df9dc434fabe2
Isuru-Kottahachchi <isurukottasl@gmail.com>
2023-07-22 08:16:07 +0530

    Update: description of changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Isuru U Can
+         Isuru Kottahachchi Associate Software Engineer
         </p>
         <a
           className="App-link"
