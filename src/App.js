0e7929a2b9dcf8df015216ffe85826726ca9088a
Isuru-Kottahachchi <isurukottasl@gmail.com>
2023-07-22 08:29:55 +0530

    Des

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Isuru Kottahachchi Associate Software Engineer
+         Isuru Kottahachchi(University Of Moratuwa)  - Associate Software Engineer
         </p>
         <a
           className="App-link"
